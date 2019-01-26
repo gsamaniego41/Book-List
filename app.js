3cf30b92a7ab86583b1cddbf7d3c1cb842e8fd07
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-01-26 14:17:27 -0800

    JS: Clear fields after submit

--- app.js
+++ app.js
@@ -40,6 +40,12 @@ class UI {
 
     list.appendChild(row);
   }
+
+  static clearFields() {
+    document.querySelector("#title").value = "";
+    document.querySelector("#author").value = "";
+    document.querySelector("#isbn").value = "";
+  }
 }
 
 // Store Class: Handles Storage
@@ -50,16 +56,19 @@ document.addEventListener("DOMContentLoaded", UI.displayBooks);
 // Event: Add a Book
 document.querySelector("#book-form").addEventListener("submit", e => {
   e.preventDefault();
-  // get form values
+  // Get form values
   const title = document.querySelector("#title").value;
   const author = document.querySelector("#author").value;
   const isbn = document.querySelector("#isbn").value;
 
-  // instantiate book
+  // Instantiate book
   const book = new Book(title, author, isbn);
   console.log(book);
 
-  // add bok to UI
+  // Add book to UI
   UI.addBookToList(book);
+
+  // Clear fields
+  UI.clearFields();
 });
 // Event: Remove a Book
