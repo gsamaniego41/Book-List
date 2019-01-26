815998ff3a6df0ff4204477a0da025a6cb04841d
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-01-26 14:10:12 -0800

    JS: Display books

---
+++ app.js
@@ -0,0 +1,52 @@
+// Book Class: Represents a Book
+class Book {
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+  }
+}
+
+// UI Class: Handle UI Tasks
+class UI {
+  static displayBooks() {
+    const storedBooks = [
+      {
+        title: "Book One",
+        author: "John Doe",
+        isbn: "343434"
+      },
+      {
+        title: "Book Two",
+        author: "Jane Doe",
+        isbn: "4545454"
+      }
+    ];
+
+    const books = storedBooks;
+
+    books.forEach(book => UI.addBookToList(book));
+  }
+
+  static addBookToList(book) {
+    const list = document.querySelector("#book-list");
+    const row = document.createElement("tr");
+    row.innerHTML = `
+      <td>${book.title}</td>
+      <td>${book.author}</td>
+      <td>${book.isbn}</td>
+      <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+    `;
+
+    list.appendChild(row);
+  }
+}
+
+// Store Class: Handles Storage
+
+// Event: Display Books
+document.addEventListener("DOMContentLoaded", UI.displayBooks);
+
+// Event: Add a Book
+
+// Event: Remove a Book
