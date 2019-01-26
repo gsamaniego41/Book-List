b4e157e6a1f0bbc229f5791c462cc62dc85cd469
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-01-26 14:15:20 -0800

    JS: Add a book to list

--- app.js
+++ app.js
@@ -48,5 +48,18 @@ class UI {
 document.addEventListener("DOMContentLoaded", UI.displayBooks);
 
 // Event: Add a Book
+document.querySelector("#book-form").addEventListener("submit", e => {
+  e.preventDefault();
+  // get form values
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const isbn = document.querySelector("#isbn").value;
 
+  // instantiate book
+  const book = new Book(title, author, isbn);
+  console.log(book);
+
+  // add bok to UI
+  UI.addBookToList(book);
+});
 // Event: Remove a Book
