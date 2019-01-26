21f973c1501a87269715bfd6d390c7d4a42d88ee
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-01-26 14:23:10 -0800

    JS: Delete a book

--- app.js
+++ app.js
@@ -41,6 +41,12 @@ class UI {
     list.appendChild(row);
   }
 
+  static deleteBook(el) {
+    if (el.classList.contains("delete")) {
+      el.parentElement.parentElement.remove();
+    }
+  }
+
   static clearFields() {
     document.querySelector("#title").value = "";
     document.querySelector("#author").value = "";
@@ -71,4 +77,8 @@ document.querySelector("#book-form").addEventListener("submit", e => {
   // Clear fields
   UI.clearFields();
 });
+
 // Event: Remove a Book
+document.querySelector("#book-list").addEventListener("click", e => {
+  UI.deleteBook(e.target);
+});
